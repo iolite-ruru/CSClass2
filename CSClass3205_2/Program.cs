3b182baeee83d48e47baf957af0d986ca3c84eed
KimSoYeon <s2023@e-mirim.hs.kr>
2022-08-25 12:07:51 +0900

    #4 24-4. 구조체

--- CSClass3205_2/Program.cs
+++ CSClass3205_2/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Xml.Schema;
 
 namespace CSClass3205_2
 {
     class Program
     {
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
+
         static void Main(string[] args)
         {
             //24-1. 제네릭
@@ -15,11 +22,13 @@ namespace CSClass3205_2
             Console.WriteLine(wantedInt.Value);
             Console.WriteLine(wantedDouble.Value);
 
+
             //24-2. 인덱서
             Products p = new Products();
             Console.WriteLine("오늘의 점심 메뉴는 " + p[2] + "입니다.");
             p[2] = "단무지";
             Console.WriteLine("오늘의 점심 메뉴는 " + p[2] + "입니다.");
+
 
             //24-3. out 키워드
             Console.WriteLine("숫자 입력: ");
@@ -38,11 +47,75 @@ namespace CSClass3205_2
             NextPos(x, y, vx, vy, out x, out y);
             Console.WriteLine("다음 좌표: (" + x + ", " + y + ")");
 
+
+            //24-4. 구조체
+            Point point;
+            point.X = 10;
+            point.y = 10;
+            point = new Point(100, 200);
+            point = new Point();
+            Console.WriteLine(point.X + " / " + point.y);
+
+            //복사: 구조체 vs 클래스
+            PointClass pcA = new PointClass(10, 20);
+            PointClass pcB = pcA;
+            pcB.x = 100;
+            pcB.y = 200;
+            Console.WriteLine("pcA: " + pcA.x + " / " + pcA.y);
+            Console.WriteLine("pcB: " + pcB.x + " / " + pcB.y);
+
+            PointStruct psA = new PointStruct(10, 20);
+            PointStruct psB = psA;
+            psB.x = 100;
+            psB.y = 200;
+            Console.WriteLine("psA: " + psA.x + " / " + psA.y);
+            Console.WriteLine("psB: " + psB.x + " / " + psB.y);
         }
-        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+
+        class PointClass
         {
-            rx = x + vx;
-            ry = y + vy;
+            public int x;
+            public int y;
+            public PointClass(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        struct PointStruct
+        {
+            public int x;
+            public int y;
+            public PointStruct(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        struct Point
+        {
+            public int X;
+            public int y;
+            public string testA; // ="test; //초기화 불가능
+            public string testB;
+
+            public Point(int x, int y)
+            {
+                this.X = x;
+                this.y = y;
+                this.testA = "초기화";
+                this.testB = "초기화";
+            }
+
+            public Point(int x, int y, string s)
+            {
+                this.X = x;
+                this.y = y;
+                this.testA = s;
+                this.testB = s;
+            }
         }
     }
 }
